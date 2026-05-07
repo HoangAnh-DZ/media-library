5c973ae1576abffa78eadd18273fb6fcc6272dc2
HoangAnh-DZ <duyluc091@gmail.com>
2026-05-08 00:17:39 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2/promisee");
 
 // Dùng biến môi trường khi deploy lên Render, fallback về local
 const pool = mysql.createPool({
